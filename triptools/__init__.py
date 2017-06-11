e8fb510aa4f15aa54f6b439336a12817f2d5a5fe
Eierkopp <depperter.depp@googlemail.com>
2017-06-11 16:43:44 +0200

    save track as gpx file

--- triptools/__init__.py
+++ triptools/__init__.py
@@ -1,5 +1,5 @@
 from .configuration import config
-from .common import Trackpoint, Feature
+from .common import Trackpoint, Feature, distance
 from .db_impl import DB
 from .map_support import MapTool
 
--- triptools/trackmap.py
+++ triptools/trackmap.py
@@ -3,10 +3,13 @@
 
 import dateutil.parser as parser
 import calendar
+from datetime import datetime
 import logging
+import os
 import sys
+import time
 
-from triptools import config
+from triptools import config, distance
 from triptools import DB
 from triptools import osm_mapper
 
@@ -17,11 +20,50 @@ def parse_ts(ts):
     dt = parser.parse(ts)
     return calendar.timegm(dt.utctimetuple())
 
-def make_trackmap(start_time, end_time):
+def write_track(track, track_name):
+    with open(track_name, "w") as outf:
+        outf.write("""<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
+    <gpx xmlns="http://www.topografix.com/GPX/1/1"
+         creator="GPSWPT_to_GPX"
+         version="1.1"
+         xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+         xsi:schemaLocation="http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd">
+    """)
+
+        previous = None
+        trackSegOpen = False
+        count = 0
+        for i in track:
+            if (not previous
+                or abs(previous.timestamp - i.timestamp) > 3600
+                or distance(previous.longitude, previous.latitude,
+                            i.longitude, i.latitude) > 5000.0):
+                print("starting new track segment at", time.strftime("%d.%m.%Y,%H:%M", time.localtime(i.timestamp)))
+                if trackSegOpen:
+                    outf.write("    </trkseg>\n")
+                    outf.write("  </trk>\n")
+                outf.write("  <trk>\n")
+                outf.write("    <name>%s_%02d</name>\n" % (os.path.splitext(os.path.basename(track_name))[0], count) )
+                outf.write("    <trkseg>\n")
+                trackSegOpen = True
+                count += 1
+
+            outf.write('      <trkpt lat="%f" lon="%f">\n        <ele>%f</ele>\n        <time>%s</time>\n      </trkpt>\n' % (i.latitude, i.longitude, i.altitude, datetime.utcfromtimestamp(i.timestamp).isoformat() + "Z") )
+            previous = i
+
+        if trackSegOpen:
+            outf.write("    </trkseg>\n")
+            outf.write("  </trk>\n")
+
+        outf.write("</gpx>\n")
+
+def make_trackmap(start_time, end_time, track_name):
     with DB() as db:
         track = db.fetch_trackpoints(start_time, end_time)
         if len(track)< 2:
             raise Exception("track too short")
+
+        write_track(track, track_name)
 
         bb = osm_mapper.get_bounding_box(track,
                                          config.getfloat("Map", "marg_pct"),
@@ -43,8 +85,9 @@ if __name__ == "__main__":
 
         start_time = parse_ts(config.get("Track", "start"))
         end_time = parse_ts(config.get("Track", "end"))
-        
-        make_trackmap(start_time, end_time)
+        track_name = config.get("Track", "name")
+
+        make_trackmap(start_time, end_time, track_name)
         
     except Exception as e:
         logging.getLogger(__name__).error(e, exc_info=True)
